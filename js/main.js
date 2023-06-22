f0e6a6f43894fabd65823b6aaae0edbe4f930092
MelissaPleitez <melissa.ramirez0pleitez@gmail.com>
2023-06-22 12:10:37 -0600

    Remove file input and clean code

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
+// Variables
 const form = document.querySelector('#form')
 const blog_container = document.querySelector('.Blog_container')
 let blog_info = []
 
 
+
+// Events
 form.addEventListener('submit',blog_verify)
 
-// document.addEventListener('DOMContentLoaded', ()=>{
-//     blog_info = JSON.parse(localStorage.getItem('blog')) || []
-//     create_blog()
-// })
+document.addEventListener('DOMContentLoaded', ()=>{
+    blog_info = JSON.parse(localStorage.getItem('blog')) || []
+    create_blog()
+})
 
 
+// Functions
 function blog_verify(e){
     e.preventDefault()
 
     const title_blog = document.querySelector('#blog_title').value
     const body_blog = document.querySelector('#blog_body').value
-    const file_blog = document.querySelector('input[type="file"]').files[0]
-
-    const reader = new FileReader()
-    reader.readAsDataURL(file_blog)
-    file_blog.src= reader.result
-
-    const inputs = {
+   
+    inputs = {
         id: Date.now(),
         title:title_blog,
         body: body_blog,
-        file: file_blog,
+
     }
 
- 
-    if(Object.values(inputs).length < 0){
+
+    if(Object.values(inputs).includes('')){
 
         alert_message('One of the input is empty, try again..')
-        console.log('empty')
         return
     }
 
     blog_info= [...blog_info, inputs]
 
-    console.log('checking..', blog_info )
+
     create_blog()
     form.reset()
 }
+
+
 
 function create_blog(){
     remove_blog()
@@ -52,7 +52,7 @@ function create_blog(){
          
             
             const btn_delete= document.createElement('i')
-            btn_delete.classList.add('remove_blog', 'bi', 'bi-trash3-fill')
+            btn_delete.classList.add('remove_blog', 'bi', 'bi-trash3-fill', 'text-danger')
 
             btn_delete.onclick=()=>{
 
@@ -60,22 +60,15 @@ function create_blog(){
             }
 
             const container= document.createElement('div')
-            const imgs = document.createElement('img')
             const p = document.createElement('p')
             const h2 = document.createElement('h2')
             h2.innerText= blog.title
             p.innerText= blog.body
-            imgs.src= URL.createObjectURL(blog.file) 
-            container.classList.add('mt-4', )
-            container.appendChild(imgs)
+
+            container.classList.add('mt-4', 'col-6', 'card', 'p-2')
             container.appendChild(h2)
             container.appendChild(p)
-
-            // container.innerHTML=`
-            // <h2>${blog.title}</h2>
-            // <p> ${blog.body}</p>
-
-            // ` 
+ 
             container.appendChild(btn_delete)
             blog_container.appendChild(container)
 
